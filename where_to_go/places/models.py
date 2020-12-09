d432db92560136a8d178a9e22b71c8a7195d00c6
Andrew <gorlov.a@mail.ru>
2020-12-09 11:27:05 +0300

    Add Place model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
-print("hello!")
+class Place (models.Model):
+    title = models.CharField(max_length=200)
+    text  = models.TextField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="places"
+    )
+    image = models.ImageField(upload_to='places/', blank=True, null=True)
+
+    def __str__(self):
+        return self.text
