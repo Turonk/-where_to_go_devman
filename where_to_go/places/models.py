adcab948045348aba60ff57780003d3eb6915aff
Andrew <gorlov.a@mail.ru>
2020-12-09 13:32:25 +0300

    Add new Place model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -6,14 +6,10 @@ User = get_user_model()
 # Create your models here.
 class Place (models.Model):
     title = models.CharField(max_length=200)
-    text  = models.TextField()
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="places"
-    )
-    image = models.ImageField(upload_to='places/', blank=True, null=True)
+    description_short  = models.TextField(max_length=300)
+    description_long = models.TextField()
+    lng = models.FloatField( blank=True, null=True)
+    lat = models.FloatField( blank=True, null=True)
 
     def __str__(self):
-        return self.text
+        return self.title
